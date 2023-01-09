6eb50f06c3d0e4ad3bcb5e557f80129114cade2a
Azkarul <118016842+Azkarul@users.noreply.github.com>
2023-01-08 18:54:10 -0800

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+$(function(){
+    $(document).ready(function(){
+        $('.carousel').carousel();
+      });
+     
+      
+    });
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: true,
+        indicators: true
+      });
